743a4865013829505a246a86f3be4a5c6e6b654e
Trey hunnicutt <33182842+treygithub@users.noreply.github.com>
2018-07-06 02:44:09 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+require ('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+const app = express();
+const products_controller = require('./products_controller');
+
+app.use(bodyParser.json());
+
+massive( process.env.CONNECTION_STRING ).then( dbInstance => {
+    app.set('db', dbInstance)
+} ).catch(err => console.log(err));
+
+
+app.post( '/api/product', products_controller.create );
+app.get( '/api/products', products_controller.getAll );
+app.get( '/api/product/:id', products_controller.getOne );
+app.put( '/api/product/:id', products_controller.update );
+app.delete( '/api/product/:id', products_controller.delete );
+
+const port = process.env.PORT || 3000;
+app.listen( port, () => { console.log(`Server listening on port ${port}.`);});
+
